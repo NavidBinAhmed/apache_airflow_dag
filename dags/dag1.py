b01bfc349983a62659ce79fb89c0f73e38d9b0df
Navid Bin Ahmed <45857107+NavidBinAhmed@users.noreply.github.com>
2025-05-02 19:22:27 +0600

    Update dag1.py

--- dags/dag1.py
+++ dags/dag1.py
@@ -6,6 +6,7 @@ from datetime import datetime
 
 '''this DAG demonstrates the workflow on an ETL pipeline'''
 
+# tasks
 def extract():
     pass
 
@@ -15,9 +16,12 @@ def transform():
 def load():
     pass
 
-with DAG("Dag1", start_date=datetime(2025,2,5), schedule="@daily", catchup=False) as dag:
+# schedule
+'''The airflow UI will show the name as it is defined here: 'dag1' '''
+with DAG("dag1", start_date=datetime(2025,2,5), schedule="@daily", catchup=False) as dag:
     t1 = PythonOperator(task_id="extract", python_callable=extract)
     t2 = PythonOperator(task_id="transform", python_callable=transform)
     t3 = PythonOperator(task_id="load", python_callable=load)
 
+# dependencies and sequence of workflow
     t1 >> t2 >> t3
